2699127c37fae71cf7b3a1d621c02c8612c43339
Lucas Rosenzweig <xeit0w@gmail.com>
2025-02-03 17:24:56 +0100

    add void RulesForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 import { useState } from 'react'
 import DomainFetcher from './components/DomainFetcher'
+import RulesForm from './components/RulesForm'
 
 function App() {
   const [domain, setDomain] = useState('')
+  const [rules, setRules] = useState([])
 
  
   return (
     <>
     <DomainFetcher domain={domain} setDomain={setDomain} />
-    
+    <RulesForm rules={rules} setRules={setRules} />
     </>
   )
 }
---
+++ src/components/RulesForm.jsx
@@ -0,0 +1,4 @@
+const RulesForm = ({ props }) => {
+};
+
+export default RulesForm;
