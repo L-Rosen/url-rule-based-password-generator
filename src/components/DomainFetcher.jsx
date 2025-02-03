79650b4acdc91beab2fe3539a4c235feff7c8996
Lucas Rosenzweig <xeit0w@gmail.com>
2025-02-03 17:20:14 +0100

    refactor DomainFetcher

--- src/components/DomainFetcher.jsx
+++ src/components/DomainFetcher.jsx
@@ -4,35 +4,46 @@ const DomainFetcher = (props) => {
 
     useEffect(() => {
         const getDomain = async () => {
-          try {
-            //Récupération de l'onglet actif
-            const [tab] = await chrome.tabs.query({active: true, currentWindow: true})
-    
-            //Récupération du domaine en supprimant tout les sous-domaines
-            if(tab?.url) {
-              const url = new URL(tab.url);
-              props.setDomain(extractMainDomain(url.hostname));
+            try {
+                const [tab] = await getActiveTab();
+
+                if (doUrlExist(tab)) {
+                    const url = new URL(tab.url);
+                    setDomainFromURL(url);
+                }
+            } catch (error) {
+                console.error(error);
             }
-          }
-          catch(error) {
-            console.error(error);
-          }
-        }
+        };
+
+        const extractMainDomain = (hostname) => {
+            const parts = hostname.split('.');
+            if (parts.length > 2) {
+                return parts.slice(-2).join('.');
+            }
+            return hostname;
+        };
+
+        const doUrlExist = (tab) => {
+            return tab?.url;
+        };
+
+        const getActiveTab = () => {
+            return chrome.tabs.query({ active: true, currentWindow: true });
+        };
+
+        const setDomainFromURL = (url) => {
+            props.setDomain(extractMainDomain(url.hostname));
+        };
+
         getDomain();
-      }, [props]);
-    
-      const extractMainDomain = (hostname) => {
-        const parts = hostname.split('.');
-        if (parts.length > 2) {
-          return parts.slice(-2).join('.'); 
-        }
-        return hostname;
-      };
+    }, [props]);
 
     return (
         <div>
             <h1>{props.domain}</h1>
-        </div>)
-    }
+        </div>
+    );
+};
 
 export default DomainFetcher;
